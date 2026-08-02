ddc085fb7d1636641815bea2cad2e81bc757b820
Emi Anyakpor Jr. <eanyakpor@gmail.com>
2026-08-01 20:29:40 -0400

    didn't struggle on this 8/1 got optimal

--- arrays/contains_duplicate.py
+++ arrays/contains_duplicate.py
@@ -1,9 +1,18 @@
-# time: O(n)
-# space: O(n)
+'''
+time
+O(N)
+space
+O(N)
+
+for worst case
+because at most we save all elements since there non unique 
+and iterate though all elements since there non unique 
+'''
 def containsDuplicate(nums):
-   mySet = set()
-   for n in nums:
-      if n in mySet:
-         return True
-      mySet.add(n)
-   return False
+    mySet = set()
+    for n in nums:
+        if n in mySet:
+            return True 
+        mySet.add(n)
+    return False
+
