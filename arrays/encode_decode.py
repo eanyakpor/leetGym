94ead8b85d3e168ebb74df3588543f46f664ff1c
Emi Anyakpor Jr. <eanyakpor@gmail.com>
2026-08-16 16:39:32 -0700

    debugged with gpt skill and fixed encode and decode followed RAMPER

--- arrays/encode_decode.py
+++ arrays/encode_decode.py
@@ -1,34 +1,66 @@
 '''
-	•	Encode: time O(m), space O(m)
-	•	Decode: time O(m), space O(m)
+RAMP
+
+R - restate the problem
+    write an algoritm for both encode to encode and decode to decode strings
+    any character could appear 
+    no libraries allwoed 
+
+A - ask questions
+    how big could the input get so any time/space fcosntraints? 
+M - make examples
+    input = ['emi', 'loves', 'chicken']
+    # could encode with 5's in between emi55555loves5555chicken
+    output = ['emi', 'loves', 'chicken']
+    
+    would be wrong because 5 is apart of 256 ascii content too 
+    meaning you could get an output of all 5's 
+P - pick a pattern 
+    two pointers or just string manipulation to be honest 
+E - explain in plain english 
+    given this example
+    input = ['emi', 'loves', 'chicken']
+    emi#3loves#5chicken#7
+               i
+                        j
+    we will encodde with the message above
+    utilize two pointers 
+    once j hits a number after # move i that many times
+    once i hits # have i and j point at the same thing + 1 to correctly move on to the next word 
+
+    output = ['emi', 'loves', 'chicken']
+R - review 
+
+
+
+    input = ['emi', 'loves', 'chicken']
+    emi#3loves#5chicken#7
+    i
+       j
 '''
-class Solution:
-
-   def encode(self,strs):
-      res = ""
-      for s in strs:
-         res += str(len(s)) + "#" + s
-      return res
-            
+def encode(strs):
+    res = ''
+    for word in strs:
+        res += (str(len(word)) + '#' + word)
+    print('encode res',res)
+    return res
 
 
-
-   def decode(self,strs):
-      res = []
-      i = 0
-
-      while i < len(s):
-         j = i
-
-      # find the '#'
-      while s[j] != "#":
-         j += 1
-
-         length = int(s[i:j])
-         word = s[j+1 : j+1+length]
-
-         res.append(word)
-
-         i = j + 1 + length
-
-      return res
+def decode(s):
+    i,j = 0,0
+    word = ''
+    res = []
+    while j < len(s):
+        while s[j] != '#':
+            j += 1
+        count = int(s[i:j])
+        i = j + 1
+        while count > 0:
+            word += s[i]
+            i += 1
+            count -= 1
+        j = i
+        res.append(word)
+        word = ''
+    print('res decode', res)
+    return res 
