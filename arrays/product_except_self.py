be7a4d5294a250d8c937b055be3f440053108732
Emi Anyakpor Jr. <eanyakpor@gmail.com>
2026-08-16 21:21:02 -0700

    struggled with opitmal and coming up with a working solution when building the prefix and suffix arrays

--- arrays/product_except_self.py
+++ arrays/product_except_self.py
@@ -1,47 +1,60 @@
-# O(n) solution
-# we want product of everything LEFT × product of everything RIGHT
-def productExceptSelf(nums):
-   front = [1] * len(nums)
-   back = [1] * len(nums)
-   ans = [0] * len(nums)
-   prod = 1
-
-   for idx in range(len(nums)):
-      front[idx] = prod
-      prod *= nums[idx]
-   print('front',front)
-   prod = 1
-
-   for idx in range(len(nums)-1,-1,-1):
-      back[idx] = prod
-      prod *= nums[idx]
-   print('back',back)
-
-   for i in range(len(front)):
-      ans[i] = (front[i] * back[i])
-
-   print('a',ans)
-   return ans 
 
 '''
-O(1) solution
-def productExceptSelf(nums):
-    n = len(nums)
-    ans = [1] * n
+RAMPER
+R - restate the problem 
+   given an array nums return a new array thats content is the product of all nums[i] constraint i the ith postioin doesn't include nums[i]
+   so 
+   [1,2,3,4]
+   ans = [24,12,8,6]
+A - ask questions 
+   don't have any questions at the top of my head for this one not going to lie 
+   leetcode platforms awsnered all my questions with its description tbh 
+   idk if i should just not read leetcdeo descript constraints and just my ask question but thene again reading the problem finding your question isn't good by reading is a good skill to have 
+M - make example 
+   [2,3,3] 
+   a = [9,6,6]
+   pretty straight forward tbh
+P - pseudocdoe
+   [2,3,3]
+   forward skip the ith current 
+   [9,3,1]
+   backward skip the ith current
+   [6,2,1]
+   a = [9,6,6]
 
-    prefix = 1
-    for i in range(n):
-        ans[i] = prefix
-        prefix *= nums[i]
+   [1,2,3,4]
+   a = [24,12,8,6]
+   [24,12,4,1]
+   [6,2,1,1]
+   a = [24,12,8,6]
+   created a forward product array that skips the current ith
+   do the same for backward
 
-    suffix = 1
-    for i in range(n - 1, -1, -1):
-        ans[i] *= suffix
-        suffix *= nums[i]
+   append the resultant awnsers as for forward array should multiple forward to the backward arrays reverse 
+   so forwardly go through forward array times it by backward array in reverse
+   return each element in new array for the awnser 
+E - is the baove explain the plan for P pick a pattern im just dfoing traverslals of arrays 
+[2,3,3]
 
-    return ans
 '''
+def productExceptSelf(nums):
+   prefix = [1] * len(nums)
+   suffix = [1] * len(nums)
+   prodPrefix = 1
+   for n in range(len(nums)):
+      prefix[n] = prodPrefix
+      prodPrefix *= nums[n]
+   prodSuffix = 1
+   for n in range(len(nums)-1,-1,-1):
+      suffix[n] = prodSuffix
+      prodSuffix *= nums[n]
+
+   res = []
+   for p,s in zip(prefix,suffix):
+      res.append(p*s)
+   return res 
+
       
-      
+
 
 
