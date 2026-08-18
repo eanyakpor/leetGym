7d7d1e62856842a00360903f86a7d217de300909
Emi Anyakpor Jr. <eanyakpor@gmail.com>
2026-08-18 15:53:44 -0700

    struggled with coming up with two pointer solution wasn't unsure to use three sum solution in makeup but shouldve just removed it as it was unnesscary

--- twoPointers/two_sum_2.py
+++ twoPointers/two_sum_2.py
@@ -1,16 +1,50 @@
-# O(n) because each pointer moves once across the array
-# Space is O(1) (you’re right there)
+'''
+RAMPER
+
+R - restate the problem 
+   given a sorted list in ascending order 
+   find twoi numbers that sum up to target 
+   result must be indicies 
+   the nums array starts at 1 instead of 0 
+   ensure indicies are respetinve their position in the array 
+   wehre the output reflects indeix i is < index j alwayus 
+A - ask question 
+   nothing right now 
+   can there be duplicates in this input ?
+M - make an example
+   [1,3,4,5]
+   target = 5
+   [1,3]
+E - explain a plan 
+   create two pointers 
+   [1,3,4,5]
+    i j
+    since its sorted adjust j to be move right if the total is < target else move j to the left 
+    theres always a unique solution for this problem 
+    to enswure we don't use the exact indcie twice 
+    check if the prev if i > 0 for i and j next thats not  the same element 
+R - review
+target = 5
+[1,3,4,5]
+ i     j
+ total = 6
+
+[1,3,4,5]
+ i   j
+
+'''
 
 def twoSum(numbers, target):
-   l,r = 0,len(numbers)-1
-   while True:
-      if numbers[l] + numbers[r] == target:
-         return [l,r]
-      if numbers[l] + numbers[r] > target:
-         r -= 1 
-      elif numbers[l] + numbers[r] < target:
-         l += 1
+   idx,j = 0,len(numbers)
+   while idx < j:
+      total = (numbers[idx] + numbers[j])
+      if total > target:
+         j -= 1
+      elif total < target:
+         idx += 1
       else:
-         l += 1
-         r -= 1
-      
+         while idx > 0 and numbers[idx] == numbers[idx-1]:
+            idx += 1
+         while idx < j and numbers[j] == numbers[j+1]:
+            j -= 1
+         return [(idx+1),(j+1)]
